6493340f8a4ae9a69017bbd1835aaa299bb6da3b
Emil Nikov <contacts@enstyled.com>
2016-04-05 13:47:35 +0300

    Add basic JS logic

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-$(function() {
+var app = {
+    api: function(endpoint) {
+        endpoint = endpoint || '';
+        return 'https://api.iconfinder.com/v2/' + endpoint;
+    },
 
+    search: function(query) {
+        $.getJSON(this.api('icons/search?query='+query), function(result) {
+            return result;
+        });
+    },
+
+    init: function() {
+        console.log("Hello");
+    }
+};
+
+$(function() {
+    app.init();
 });

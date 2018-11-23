5956099a3767de39f69a9dd5335a26bf2e13f75e
Emil Nikov <contacts@enstyled.com>
2018-11-23 10:59:00 +0200

    JS formatting

--- node-server/index.js
+++ node-server/index.js
@@ -5,18 +5,15 @@
  * If you haven't registered an app yet, go to https://www.iconfinder.com/account/applications
  */
 
-
-
 var express = require('express');
 var app = express();
 var request = require('request');
-
 
 app.use(express.static('../public'));
 
 app.get('/token', function (req, res) {
 
-  	const nf_token_url = "https://www.iconfinder.com/api/v3/oauth2/token";
+  	var nf_token_url = "https://www.iconfinder.com/api/v3/oauth2/token";
 
   	request.post(nf_token_url, {
         form: {
@@ -27,7 +24,7 @@ app.get('/token', function (req, res) {
     }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
 			console.log(body);
-			delete body["token_type"];
+			delete body.token_type;
 			res.send(body);
 		} else {
 			console.log(error, response, body);

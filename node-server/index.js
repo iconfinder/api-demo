16e09aa3a2818f2c7474cf2399b4c4b380190949
Emil Nikov <contacts@enstyled.com>
2020-05-12 16:21:44 +0200

    Use environment variables in Node.js example

--- node-server/index.js
+++ node-server/index.js
@@ -9,20 +9,22 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+var CLIENT_ID = process.env.CLIENT_ID;
+var CLIENT_SECRET = process.env.CLIENT_SECRET;
+
 app.use(express.static('../public'));
 
 app.get('/token', function (req, res) {
-
-  	var nf_token_url = "https://www.iconfinder.com/api/v3/oauth2/token";
+	var nf_token_url = "https://www.iconfinder.com/api/v3/oauth2/token";
 
   	request.post(nf_token_url, {
-        form: {
+	        form: {
 			grant_type:'jwt_bearer',
-			client_id: 'INSERT_CLIENT_ID',
-			client_secret: 'INSERT_CLIENT_SECRET'
-	    }
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+			client_id: CLIENT_ID,
+			client_secret: CLIENT_SECRET
+		    }
+	}, function (error, response, body) {
+        	if (!error && response.statusCode == 200) {
 			console.log(body);
 			delete body.token_type;
 			res.send(body);
@@ -30,9 +32,8 @@ app.get('/token', function (req, res) {
 			console.log(error, response, body);
 		}
 	});
-
 });
 
 app.listen(3334, function () {
-  console.log('Example app listening on port 3334!');
+	console.log('Example app listening on port 3334!');
 });

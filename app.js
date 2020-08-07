5751207243f188e3cf4fea7a972b687d08728ad7
Emil Nikov <contacts@enstyled.com>
2020-08-07 14:12:13 +0200

    Cleanup

--- app.js
+++ app.js
@@ -146,22 +146,6 @@ _.templateSettings = {
     escape: /\{\{-(.*?)\}\}/g
 };
 
-$.fn.serializeObject = function() {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
-
 $(function() {
     app.init();
 });
